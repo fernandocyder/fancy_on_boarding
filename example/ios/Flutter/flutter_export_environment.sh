9d85010e8deb3243806184e4b39f64eab88bdd36
Fernando Karnagi <fernando@cyder.com.sg>
2022-01-23 14:44:57 +0800

    update

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/xsahil03x/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/xsahil03x/AndroidStudioProjects/fancy_on_boarding/example"
+export "FLUTTER_ROOT=/Users/fernandokarnagi/Work/Apps/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/fernandokarnagi/Work/Workspace/WorkspacePerformanz/fancy_on_boarding/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
